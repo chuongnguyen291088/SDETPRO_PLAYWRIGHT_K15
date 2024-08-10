38851557bc283097efd88a267578e3d3b5ca65a6
chuong nguyen <ndc.vn291088@gmail.com>
2024-08-10 11:46:17 +0700

    improve lab 6.1 & 6.2

--- labs/lab_06_1.js
+++ labs/lab_06_1.js
@@ -1,8 +1,8 @@
 const { getUserInput } = require("../labUtils/inputUtil");
 const url = "https://jsonplaceholder.typicode.com";
 
-filterUserResourcesByUserId(getUserInput(true, "Please input userId: "));
-gettingResourceDetail(getUserInput(true, "Please input resourceId: "));
+
+/**
 
 function convertJsonToObject(response) {
    return response.json();
@@ -26,4 +26,44 @@ function gettingResourceDetail(resourceId) {
       .then(function (obj) {
          return console.log(`\nPost content of resourceId ${resourceId}: \n${obj.body}\n`);
       })
+};
+ 
+ */
+
+getPost();
+
+function getPost() {
+   const userId = getUserInput(true, 'Please input userId: ');
+   getPostListByUserId(userId)
+      .then((postList) => {
+         if (postList.length === 0) {
+            throw new Error('This userId is NOT existing');
+         };
+         console.log(postList);
+         return postList;
+      })
+      .then(postList => {
+         return getPostByResourceId(postList);
+      })
+      .then((post) => {
+         if (post) {
+            console.log(`\nPost content: \n${post.body}\n`)
+         } else {
+            console.error(`This resourceId is NOT existing with userId ${userId}`)
+         }
+      }
+      )
+      .catch((error) =>
+         console.error(error))
+};
+
+function getPostListByUserId(userId) {
+   const filterAllPostsByUserIdUrl = url.concat(`/posts?userId=${userId}`);
+   return fetch(filterAllPostsByUserIdUrl)
+      .then((response) => response.json());
+};
+
+function getPostByResourceId(postList) {
+   const resourceId = getUserInput(true, 'Please input resourceId: ');
+   return postList.find((post) => post.id === resourceId);
 };
--- labs/lab_06_2.js
+++ labs/lab_06_2.js
@@ -1,6 +1,8 @@
 const { getUserInput } = require("../labUtils/inputUtil");
 const url = "https://jsonplaceholder.typicode.com";
 
+/**
+ * 
 filterUserResourcesByUserId(getUserInput(true, "Please input userId: "));
 gettingResourceDetail(getUserInput(true, "Please input resourceId: "));
 
@@ -16,4 +18,39 @@ async function gettingResourceDetail(resourceId) {
    const response = await fetch(getAllResourcesUrl);
    const jsonObject = await response.json();
    return console.log(`\nPost content of resourceId ${resourceId}: \n${jsonObject.body}\n`);
+};
+
+ */
+
+getPost();
+
+async function getPost() {
+   try {
+      const userId = getUserInput(true, 'Please input userId: ');
+      const postList = await getPostListByUserId(userId);
+      if (postList.length === 0) {
+         throw new Error('This userId is NOT existing');
+      };
+      console.log(postList);
+
+      const post = await getPostByResourceId(postList);
+      if (post) {
+         console.log(`\nPost content: \n${post.body}\n`)
+      } else {
+         console.error(`This resourceId is NOT existing with userId ${userId}`)
+      };
+   } catch (error) {
+      console.error(error);
+   };
+};
+
+function getPostListByUserId(userId) {
+   const filterAllPostsByUserIdUrl = url.concat(`/posts?userId=${userId}`);
+   return fetch(filterAllPostsByUserIdUrl)
+      .then((response) => response.json());
+};
+
+function getPostByResourceId(postList) {
+   const resourceId = getUserInput(true, 'Please input resourceId: ');
+   return postList.find((post) => post.id === resourceId);
 };
